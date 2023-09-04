7c7dd50523dafe04393cba00b0f0aea53f7545c8
Scottisaacson23 <40648540+Scottisaacson23@users.noreply.github.com>
2023-09-04 03:41:08 +0000

    First commit + About Me Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>About Me</h1>
+        <p>Hello, I'm [Your Name].</p>
+        <p>I am a [Your Profession] with expertise in [Your Skills].</p>
+        <p>
+          Here are some things about me:
+        </p>
+        <ul>
+          <li>I have [X] years of experience in [Field].</li>
+          <li>I am passionate about [Your Interests/Hobbies].</li>
+          <li>[Any other interesting fact about you]</li>
+        </ul>
+      </header>
+    </div>
+  );
 }
 
 export default App;
