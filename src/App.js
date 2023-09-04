2f2591378ec989c784f7bf811a3f97efa814936d
Scottisaacson23 <40648540+Scottisaacson23@users.noreply.github.com>
2023-09-04 18:29:51 +0000

    Updated Bable dependency to get rid of error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
 // Material Kit 2 React themes
-import theme from "./assets/theme";
+import theme from "assets/theme";
 
 function App() {
   <ThemeProvider theme={theme}>
